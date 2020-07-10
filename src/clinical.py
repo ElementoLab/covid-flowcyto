3a307ad6c742b6898e99190b115e6d666f26e509
afrendeiro <afrendeiro@gmail.com>
2020-07-10 19:21:35 -0400

    revamp clinical analysis

--- src/clinical.py
+++ src/clinical.py
@@ -4,78 +4,93 @@
 Visualizations of the cohort and the associated clinical data
 """
 
-from pathlib import Path
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import imc
+from src.conf import *
 
 
-def minmax_scale(x):
-    with np.errstate(divide="ignore", invalid="ignore"):
-        return (x - x.min()) / (x.max() - x.min())
-
-
-figkws = dict(dpi=300, bbox_inches="tight")
-
-original_dir = Path("data") / "original"
-metadata_dir = Path("metadata")
-data_dir = Path("data")
-results_dir = Path("results")
 output_dir = results_dir / "clinical"
 output_dir.mkdir(exist_ok=True, parents=True)
 
-for _dir in [original_dir, metadata_dir, data_dir, results_dir]:
-    _dir.mkdir(exist_ok=True, parents=True)
 
-metadata_file = metadata_dir / "annotation.pq"
-matrix_file = data_dir / "matrix.pq"
-matrix_imputed_file = data_dir / "matrix_imputed.pq"
-
-
-categories = ["patient", "severity_group", "intubated", "death", "heme", "bmt", "obesity"]
-continuous = ["timepoint"]
-variables = categories + continuous
+variables = CATEGORIES + CONTINUOUS
 
 
 meta = pd.read_parquet(metadata_file)
-matrix = pd.read_parquet(matrix_imputed_file)
-
-cols = matrix.columns.str.extract("(.*)/(.*)")
-cols.index = matrix.columns
-parent_population = cols[1].rename("parent_population")
 
 
 # Variable correlation
 
-to_corr = meta.drop_duplicates(subset="patient_code").copy()
+to_corr = meta.drop_duplicates(subset="patient_code")[variables].copy()
 
-for col in meta.columns[meta.dtypes == "category"]:
+for col in to_corr.columns[to_corr.dtypes == "category"]:
     to_corr[col] = meta[col].cat.codes
 
-for col in meta.columns[meta.dtypes.apply(lambda x: x.name).str.contains("datetime")]:
-    to_corr[col] = minmax_scale(meta[col])
+for col in to_corr.columns[
+    to_corr.dtypes.apply(lambda x: x.name).str.contains("datetime")
+]:
+    to_corr[col] = minmax_scale(to_corr[col])
 
 corrs = to_corr.corr(method="spearman").drop("patient", 0).drop("patient", 1)
 
-grid = sns.clustermap(corrs, center=0, cmap="RdBu_r", cbar_kws=dict(label="Spearman correlation"),)
-grid.savefig(output_dir / "clinial_parameters.parameter_correlation.svg", **figkws)
+
+kwargs = dict(
+    # center=0,
+    cmap="RdBu_r",
+    cbar_kws={"label": "Spearman correlation"},
+    square=True,
+)
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+sns.heatmap(corrs, ax=ax, **kwargs)
+fig.savefig(
+    output_dir / "clinial_parameters.parameter_correlation.heatmap.svg",
+    **figkws
+)
+
+
+# first do the correlation with nan filled
+grid = sns.clustermap(corrs.fillna(np.nanmean(corrs.values)), **kwargs)
+# then plot with heatmap ordered to still display NaNs
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+sns.heatmap(
+    corrs.iloc[
+        grid.dendrogram_row.reordered_ind, grid.dendrogram_col.reordered_ind
+    ],
+    ax=ax,
+    **kwargs
+)
+fig.savefig(
+    output_dir / "clinial_parameters.parameter_correlation.clustermap.svg",
+    **figkws
+)
 
 # Patient correlation in clinical variables only
-
-
+p_corr = to_corr.query("COVID19 == 1").loc[:, corrs.index]
+# # quickly impute missing batch for three patients
+for col in p_corr.columns[p_corr.isnull().any()]:
+    p_corr.loc[p_corr[col].isnull(), col] = p_corr[col].dropna().mean()
 grid = sns.clustermap(
-    to_corr.query("patient == 1").loc[:, corrs.index].T.corr(method="spearman"),
-    center=0,
+    p_corr.T.corr(method="spearman"),
+    center=0.5,
     cmap="RdBu_r",
     cbar_kws=dict(label="Spearman correlation"),
     xticklabels=True,
     yticklabels=True,
+    row_colors=sample_variables,
+    colors_ratio=0.15 / sample_variables.shape[1],
+    figsize=(12, 11),
 )
 grid.savefig(output_dir / "clinial_parameters.sample_correlation.svg", **figkws)
+
+
+# Get Supl Fig. 1 / Table
+# # Summary stats
+# # Test for imbalances in clinical data between severity groups
+
 
 # Add lock file
 open(output_dir / "__done__", "w")
